69c712c39facf69b5a0136feeaf749e0b1f98259
Mradu Raj Singh Khichi <fitnotize@gmail.com>
2023-09-12 11:05:57 +0530

    shivam

--- generateSitemap.js
+++ generateSitemap.js
@@ -2,56 +2,54 @@
 
 const fs = require('fs');
 const axios = require('axios');
-const { SitemapStream, streamToPromise } = require('sitemap');
 
 async function generateSitemap() {
   try {
-    // Fetch data from your API and generate the sitemap XML here
+    // Fetch data from your APIs and generate the sitemap XML here
     const apiResponse = await axios.get('https://api.spacite.com/api/workSpace');
 
     // Extract URLs from the API response (adjust this based on your API response structure)
-    const urls = apiResponse.data.map(item => ({
-      url: `/coworking/${item.slug}`,
-      changefreq: 'weekly',
-      priority: 0.7,
-    }));
+    const workspaceUrls = apiResponse.data.map(item => `https://spacite.com/coworking/${item.slug}`);
+
     const cityApiResponse = await axios.get('https://api.spacite.com/api/city/active-cities');
 
     // Extract city URLs from the city API response (adjust this based on your API response structure)
-    const cityUrls = cityApiResponse.data.map(item => ({
-      url: `/coworking-space/${item.name.toLowerCase()}`,
-      changefreq: 'weekly',
-      priority: 0.6,
-    }));
+    const cityUrls = cityApiResponse.data.map(item => `https://spacite.com/coworking-space/${item.name.toLowerCase()}`);
 
-    // Fetch data from the microlocation API endpoint (replace with your microlocation API endpoint)
     const microlocationApiResponse = await axios.get('https://api.spacite.com/api/microlocation/active-location');
 
     // Extract microlocation URLs from the microlocation API response (adjust this based on your API response structure)
-    const microlocationUrls = microlocationApiResponse.data.map(item => ({
-      url: `/coworking-space/${item.city.name.toLowerCase()}/${item.name.replace(/\s+/g, '-').toLowerCase()}`,
-      changefreq: 'weekly',
-      priority: 0.5,
-    }));
-    // Create a sitemap
-    const stream = new SitemapStream({ hostname: 'https://spacite.com' });
+    const microlocationUrls = microlocationApiResponse.data.map(item => `https://spacite.com/coworking-space/${item.city.name.toLowerCase()}/${item.name.replace(/\s+/g, '-').toLowerCase()}`);
 
-    // Add URLs to the sitemap
-    urls.forEach(url => {
-      stream.write(url);
-    });
-
-    cityUrls.forEach(url => {
-      stream.write(url);
-    });
-    microlocationUrls.forEach(url => {
-      stream.write(url);
-    });
-
-    stream.end();
-
-    // Generate the sitemap as a string
-    const sitemap = await streamToPromise(stream);
+    // Create a sitemap as a plain text string
+    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" 
+xmlns:news="http://www.google.com/schemas/sitemap-news/0.9"
+xmlns:xhtml="http://www.w3.org/1999/xhtml"
+xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"
+xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
+  ${workspaceUrls.map(url => `
+    <url>
+      <loc>${url}</loc>
+      <changefreq>weekly</changefreq>
+      <priority>0.7</priority>
+    </url>
+  `).join('')}
+  ${cityUrls.map(url => `
+    <url>
+      <loc>${url}</loc>
+      <changefreq>weekly</changefreq>
+      <priority>0.6</priority>
+    </url>
+  `).join('')}
+  ${microlocationUrls.map(url => `
+    <url>
+      <loc>${url}</loc>
+      <changefreq>weekly</changefreq>
+      <priority>0.5</priority>
+    </url>
+  `).join('')}
+</urlset>`;
 
     // Save the sitemap to the root folder of the React project
     fs.writeFileSync('./public/sitemap.xml', sitemap, 'utf-8');
