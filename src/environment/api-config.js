2fa669674c17bbb8c7b026b6e2f8a8a4b5142ed1
Mradu Raj Singh Khichi <fitnotize@gmail.com>
2023-10-10 17:47:28 +0530

    shivam

--- src/environment/api-config.js
+++ src/environment/api-config.js
@@ -1,4 +1,4 @@
-const baseUrl = "http://localhost:8001";
-// const baseUrl = "https://api.spacite.com";
+// const baseUrl = "http://localhost:8001";
+const baseUrl = "https://api.spacite.com";
 
 export default baseUrl;
